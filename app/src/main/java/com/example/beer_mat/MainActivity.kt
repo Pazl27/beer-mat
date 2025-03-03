f05597642d99310bbfaed8b983a5d2bca53e4e1d
Paul <jaul.ponas@gmail.com>
2025-03-03 17:41:26 +0100

    simple tabs

--- app/src/main/java/com/example/beer_mat/MainActivity.kt
+++ app/src/main/java/com/example/beer_mat/MainActivity.kt
@@ -6,11 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.beer_mat.ui.theme.BeerMatTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +19,61 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BeerMatTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen() {
+    var selectedTabIndex by remember { mutableIntStateOf(0) }
+    val tabs = listOf("Food", "Drinks", "Members")
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            TabRow(
+                selectedTabIndex = selectedTabIndex,
+                modifier = Modifier.padding(top = 10.dp)
+            ) {
+                tabs.forEachIndexed { index, title ->
+                    Tab(
+                        selected = selectedTabIndex == index,
+                        onClick = { selectedTabIndex = index },
+                        text = { Text(title) }
+                    )
+                }
+            }
+        }
+    ) { innerPadding ->
+        when (selectedTabIndex) {
+            0 -> FoodScreen(Modifier.padding(innerPadding))
+            1 -> DrinksScreen(Modifier.padding(innerPadding))
+            2 -> MembersScreen(Modifier.padding(innerPadding))
+        }
+    }
+}
+
+@Composable
+fun FoodScreen(modifier: Modifier = Modifier) {
+    Text(text = "Food Content", modifier = modifier)
+}
+
+@Composable
+fun DrinksScreen(modifier: Modifier = Modifier) {
+    Text(text = "Drinks Content", modifier = modifier)
+}
+
+@Composable
+fun MembersScreen(modifier: Modifier = Modifier) {
+    Text(text = "Members Content", modifier = modifier)
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MainScreenPreview() {
     BeerMatTheme {
-        Greeting("Android")
+        MainScreen()
     }
 }
