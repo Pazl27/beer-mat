1f157023c5a8058f6c73021dee7f57e7e660a169
Paul <jaul.ponas@gmail.com>
2025-03-10 20:12:59 +0100

    little upsi

--- app/src/main/java/com/example/beer_mat/database/Entities.kt
+++ app/src/main/java/com/example/beer_mat/database/Entities.kt
@@ -1,32 +1,25 @@
 package com.example.beer_mat.database
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.Query
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
-@Dao
-interface FoodDao {
-    @Insert
-    suspend fun insert(foodItem: FoodItem)
+@Entity(tableName = "food")
+data class FoodItem(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val name: String,
+    val price: Double
+)
 
-    @Query("SELECT * FROM food")
-    suspend fun getAllFoodItems(): List<FoodItem>
-}
+@Entity(tableName = "drinks")
+data class DrinkItem(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val name: String,
+    val price: Double
+)
 
-@Dao
-interface DrinkDao {
-    @Insert
-    suspend fun insert(drinkItem: DrinkItem)
-
-    @Query("SELECT * FROM drinks")
-    suspend fun getAllDrinkItems(): List<DrinkItem>
-}
-
-@Dao
-interface MemberDao {
-    @Insert
-    suspend fun insert(member: Member)
-
-    @Query("SELECT * FROM members")
-    suspend fun getAllMembers(): List<Member>
-}
+@Entity(tableName = "members")
+data class Member(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val name: String,
+    val amountToPay: Double
+)
