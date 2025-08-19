c0ea1cddc138f21fcfcd16d62a0046824e0206dc
Manu <22homa1bif@hft-stuttgart.de>
2025-08-19 18:48:49 +0200

    updated default items to work with new enum category

--- src/db/addDefaultItems.ts
+++ src/db/addDefaultItems.ts
@@ -1,26 +1,27 @@
 import { ExpoSQLiteDatabase } from "drizzle-orm/expo-sqlite";
 import { createItem } from "./dbFunctions";
 import { ItemType } from "@/types";
+import { DrinkCategory, FoodCategory } from "@/types/category";
 import { AsyncStorage } from "node_modules/expo-sqlite/build/Storage";
 
 const defaultItems = [
   // Drinks
-  { name: 'Bier', price: 2.50, category: 'Bier', info: 'Flasche, 0,5l', type: ItemType.Drink },
-  { name: 'Radler', price: 2.50, category: 'Bier', info: 'Flasche, 0,5l', type: ItemType.Drink },
-  { name: 'Alkoholfreies Bier', price: 2.50, category: 'Bier', info: 'Flasche, 0,5l', type: ItemType.Drink },
-  { name: 'Alkoholfreies Radler', price: 2.50, category: 'Bier', info: 'Flasche, 0,5l', type: ItemType.Drink },
-  { name: 'Mineralwasser', price: 1.50, category: 'Softdrinks', info: 'Flasche, 0,5l', type: ItemType.Drink },
-  { name: 'Cola Mix', price: 2.00, category: 'Softdrinks', info: 'Flasche, 0,5l', type: ItemType.Drink },
-  { name: 'Iso Sport', price: 2.00, category: 'Softdrinks', info: 'Flasche, 0,5l', type: ItemType.Drink },
-  { name: 'Bio Apfel-Birnen-Schorle', price: 2.00, category: 'Softdrinks', info: 'Flasche, 0,5l', type: ItemType.Drink },
-  { name: 'Kaffee (Tasse)', price: 1.50, category: 'Heißgetränke', info: 'mit/ohne Zucker, mit/ohne Milch', type: ItemType.Drink },
+  { name: 'Bier', price: 2.50, category: DrinkCategory.Bier, info: 'Flasche, 0,5l', type: ItemType.Drink },
+  { name: 'Radler', price: 2.50, category: DrinkCategory.Bier, info: 'Flasche, 0,5l', type: ItemType.Drink },
+  { name: 'Alkoholfreies Bier', price: 2.50, category: DrinkCategory.Bier, info: 'Flasche, 0,5l', type: ItemType.Drink },
+  { name: 'Alkoholfreies Radler', price: 2.50, category: DrinkCategory.Bier, info: 'Flasche, 0,5l', type: ItemType.Drink },
+  { name: 'Mineralwasser', price: 1.50, category: DrinkCategory.Softdrinks, info: 'Flasche, 0,5l', type: ItemType.Drink },
+  { name: 'Cola Mix', price: 2.00, category: DrinkCategory.Softdrinks, info: 'Flasche, 0,5l', type: ItemType.Drink },
+  { name: 'Iso Sport', price: 2.00, category: DrinkCategory.Softdrinks, info: 'Flasche, 0,5l', type: ItemType.Drink },
+  { name: 'Bio Apfel-Birnen-Schorle', price: 2.00, category: DrinkCategory.Softdrinks, info: 'Flasche, 0,5l', type: ItemType.Drink },
+  { name: 'Kaffee (Tasse)', price: 1.50, category: DrinkCategory.Heissgetraenke, info: 'mit/ohne Zucker, mit/ohne Milch', type: ItemType.Drink },
 
   // Food
-  { name: 'Hot Dog', price: 2.00, category: 'Hauptgericht', type: ItemType.Food },
-  { name: 'Bratwurst', price: 2.00, category: 'Hauptgericht', type: ItemType.Food },
-  { name: 'Paar Bratwürste', price: 3.00, category: 'Hauptgericht', type: ItemType.Food },
-  { name: 'Steak', price: 3.50, category: 'Hauptgericht', type: ItemType.Food },
-  { name: 'Kuchen', price: 1.00, category: 'Nachspeise', type: ItemType.Food },
+  { name: 'Hot Dog', price: 2.00, category: FoodCategory.Hauptgericht, type: ItemType.Food },
+  { name: 'Bratwurst', price: 2.00, category: FoodCategory.Hauptgericht, type: ItemType.Food },
+  { name: 'Paar Bratwürste', price: 3.00, category: FoodCategory.Hauptgericht, type: ItemType.Food },
+  { name: 'Steak', price: 3.50, category: FoodCategory.Hauptgericht, type: ItemType.Food },
+  { name: 'Kuchen', price: 1.00, category: FoodCategory.Nachspeise, type: ItemType.Food },
 ];
 
 export const addDefaultItems = async (db: ExpoSQLiteDatabase) => {
@@ -33,6 +34,8 @@ export const addDefaultItems = async (db: ExpoSQLiteDatabase) => {
         name: item.name,
         type: item.type,
         price: Math.round(item.price * 100), // convert to cents
+        info: item.info,
+        category: item.category
       });
     }
 
