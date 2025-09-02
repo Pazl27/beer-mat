b2f025de6caaacfaa849ff9b05646ba51ba12c4c
Pazl <130466427+Pazl27@users.noreply.github.com>
2025-09-02 18:17:50 +0200

    new migrations

--- drizzle/0000_mysterious_havok.sql
+++ drizzle/0000_mysterious_havok.sql
@@ -2,7 +2,7 @@ CREATE TABLE `history` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`user_id` integer,
 	`item_id` integer,
-	`timestamp` integer DEFAULT 1755609859897 NOT NULL,
+	`timestamp` integer NOT NULL,
 	`paid` integer NOT NULL,
 	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE cascade,
 	FOREIGN KEY (`item_id`) REFERENCES `items`(`id`) ON UPDATE no action ON DELETE cascade
@@ -22,6 +22,9 @@ CREATE TABLE `user_items` (
 	`user_id` integer NOT NULL,
 	`item_id` integer NOT NULL,
 	`price_per_item` integer NOT NULL,
+	`item_name` text NOT NULL,
+	`item_type` text NOT NULL,
+	`item_price` integer NOT NULL,
 	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE cascade,
 	FOREIGN KEY (`item_id`) REFERENCES `items`(`id`) ON UPDATE no action ON DELETE cascade
 );
